675e73d82381c4208a6e4b6d5a1fd4e32fe17f10
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2020-06-24 17:40:13 +0100

    Use the correct api url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const apiUrl = 'http://localhost:4010';
+const apiUrl = 'https://link-checker-api.uc.r.appspot.com/';
 
 class App extends React.Component {
   constructor() {
